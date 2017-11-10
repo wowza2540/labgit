e9a86bb5f4927323cd644627d57a72fbfa51b46f
CSTUROOM111 <CSTUROOM111@CSTURoom111-25>
2017-11-10 12:36:16 +0700

    add junit test

---
+++ LabGit/src/controller/SongsakdiTest.java
@@ -0,0 +1,14 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SongsakdiTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
